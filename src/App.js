8cdb03b4f4466082097693d623ba5e8724964871
utmalik <utmalik@amazon.com>
2023-06-22 22:02:53 +0530

    admin page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router , Routes , Route} from "react-router-dom";
 import Auth from './pages/Auth/Auth';
 import LandingPage from './pages/LandingPage/LandingPage';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
+import Admin from './pages/Admin/Admin';
 
 function App() {
   return (
@@ -11,12 +12,12 @@ function App() {
       <Router>
 
         <Routes>
-
+          
           <Route exact path="/login" element={<Auth/>}/>
           <Route exact path="/register" />
           <Route exact path="/" element={<LandingPage/>} />
-
           <Route exact path="/movie/:movieId/details" element={ <MovieDetails/>} />
+          <Route exact path="/admin" element={ <Admin/>} />
 
         </Routes>
 
